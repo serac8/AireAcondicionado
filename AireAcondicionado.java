bb24cb6c9e1f273f1efadd6d223959a4209baebc
serac8 <92148837+serac8@users.noreply.github.com>
2021-11-24 13:01:53 +0100

    0497.02 Subir y bajar temperatura

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -1,6 +1,26 @@
 public class AireAcondicionado{
 
 
+    private double temperatura;
+    
+    
+    public AireAcondicionado(){
+    
+    temperatura = 15.0;
+    
+    }
+    
+    public void subirTemperatura(){
+        temperatura = temperatura + 5;
+    }
+    
+    public void bajarTemperatura(){
+        temperatura = temperatura - 5;
+    }
+    
+    public double getTemperatura(){
+        return temperatura;
+    }
 
 
 
