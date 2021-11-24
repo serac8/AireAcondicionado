c199f48d0572da637d461b5a1fa8c6c2ad06bea5
unknown <scampo1997@gmail.com>
2021-11-24 16:35:15 +0100

    0497.04 Estadísticas y número de cambios de temperatura

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -5,29 +5,33 @@ public class AireAcondicionado{
     private double min;
     private double max;
     private double incremento;
-    
-    
+    private int vecesCambioTemperatura;
+   
     public AireAcondicionado(double tempMinima, double tempMaxima){
         temperatura = 15.0;
         min = tempMinima;
         max = tempMaxima;
         incremento = 5.0;
+        vecesCambioTemperatura = 0;
+        
     }
-    
+   
     public void subirTemperatura(){
         
-        if(temperatura + incremento < max){
+        if(temperatura + incremento <= max){
             temperatura = temperatura + incremento;
-                      
+                     
         }
-        
+        vecesCambioTemperatura = vecesCambioTemperatura + 1;
     }
-    
+   
     public void bajarTemperatura(){
-        if(temperatura - incremento > min){
+        
+        if(temperatura - incremento >= min){
             temperatura = temperatura - incremento;
-            
+           
         }
+        vecesCambioTemperatura = vecesCambioTemperatura + 1;
     }
 
     public double getTemperatura(){
@@ -39,30 +43,17 @@ public class AireAcondicionado{
             incremento = incrementoDeTemperatura;
 
         }
-        else {
-            incrementoDeTemperatura = incremento;
-            
-        }
-        
-        
-        
-        
+   
     }
 
+    public void mostrarEstadisticas(){
+        System.out.println("Temperatura actual: " + temperatura + "º" + ". Funcionamineto entre " + max + "º y " + min + "º. Temp. Máx.: " + max + "º. Temp. Mín.: " + min + "º" + ". Número de cambios de temperatura: " + vecesCambioTemperatura + ".");
 
+       
+       
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+   
 
 
 
